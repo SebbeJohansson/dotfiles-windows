db0f711aabde4a1859ab325b5f56f88065e021bc
Dragon Sebastian Johansson <sebastian.johansson@revolutionrace.se>
2026-03-10 09:40:16 +0100

    Key binds for wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -20,5 +20,24 @@ config.color_scheme = 'AdventureTime'
 config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = true
 
+-- Keys
+config.keys = {
+  -- Move focus between panes (CTRL+SHIFT)
+  { key = 'LeftArrow',  mods = 'CTRL|SHIFT', action = wezterm.action.ActivatePaneDirection 'Left' },
+  { key = 'RightArrow', mods = 'CTRL|SHIFT', action = wezterm.action.ActivatePaneDirection 'Right' },
+  { key = 'UpArrow',    mods = 'CTRL|SHIFT', action = wezterm.action.ActivatePaneDirection 'Up' },
+  { key = 'DownArrow',  mods = 'CTRL|SHIFT', action = wezterm.action.ActivatePaneDirection 'Down' },
+
+  -- Split panes (CTRL+SHIFT+ALT)
+  { key = 'LeftArrow',  mods = 'CTRL|SHIFT|ALT', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+  { key = 'DownArrow',  mods = 'CTRL|SHIFT|ALT', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },
+
+  -- Resize panes (CTRL+ALT)
+  { key = 'LeftArrow',  mods = 'CTRL|ALT', action = wezterm.action.AdjustPaneSize { 'Left', 1 } },
+  { key = 'RightArrow', mods = 'CTRL|ALT', action = wezterm.action.AdjustPaneSize { 'Right', 1 } },
+  { key = 'UpArrow',    mods = 'CTRL|ALT', action = wezterm.action.AdjustPaneSize { 'Up', 1 } },
+  { key = 'DownArrow',  mods = 'CTRL|ALT', action = wezterm.action.AdjustPaneSize { 'Down', 1 } },
+}
+
 -- Finally, return the configuration to wezterm:
 return config
