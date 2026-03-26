6ec96528d7cafb1f219abbdc491c6eca1787b231
Dragon Sebastian Johansson <sebastian.johansson@revolutionrace.se>
2026-03-26 16:06:26 +0100

    wezterm update

--- .wezterm.lua
+++ .wezterm.lua
@@ -23,20 +23,27 @@ config.use_fancy_tab_bar = true
 -- Keys
 config.keys = {
   -- Move focus between panes (CTRL+SHIFT)
-  { key = 'LeftArrow',  mods = 'CTRL|SHIFT', action = wezterm.action.ActivatePaneDirection 'Left' },
-  { key = 'RightArrow', mods = 'CTRL|SHIFT', action = wezterm.action.ActivatePaneDirection 'Right' },
-  { key = 'UpArrow',    mods = 'CTRL|SHIFT', action = wezterm.action.ActivatePaneDirection 'Up' },
-  { key = 'DownArrow',  mods = 'CTRL|SHIFT', action = wezterm.action.ActivatePaneDirection 'Down' },
+  { key = 'LeftArrow',  mods = 'SHIFT', action = wezterm.action.ActivatePaneDirection 'Left' },
+  { key = 'RightArrow', mods = 'SHIFT', action = wezterm.action.ActivatePaneDirection 'Right' },
+  { key = 'UpArrow',    mods = 'SHIFT', action = wezterm.action.ActivatePaneDirection 'Up' },
+  { key = 'DownArrow',  mods = 'SHIFT', action = wezterm.action.ActivatePaneDirection 'Down' },
 
   -- Split panes (CTRL+SHIFT+ALT)
-  { key = 'LeftArrow',  mods = 'CTRL|SHIFT|ALT', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
-  { key = 'DownArrow',  mods = 'CTRL|SHIFT|ALT', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },
+  { key = 'LeftArrow',  mods = 'CTRL', action = wezterm.action.SplitPane { direction = 'Left'  } },
+  { key = 'RightArrow', mods = 'CTRL', action = wezterm.action.SplitPane { direction = 'Right' } },
+  { key = 'DownArrow',  mods = 'CTRL', action = wezterm.action.SplitPane { direction = 'Down'  } },
+  { key = 'UpArrow',    mods = 'CTRL', action = wezterm.action.SplitPane { direction = 'Up'    } },
+  {
+    key = 'w',
+    mods = 'CTRL',
+    action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
 
   -- Resize panes (CTRL+ALT)
-  { key = 'LeftArrow',  mods = 'CTRL|ALT', action = wezterm.action.AdjustPaneSize { 'Left', 1 } },
-  { key = 'RightArrow', mods = 'CTRL|ALT', action = wezterm.action.AdjustPaneSize { 'Right', 1 } },
-  { key = 'UpArrow',    mods = 'CTRL|ALT', action = wezterm.action.AdjustPaneSize { 'Up', 1 } },
-  { key = 'DownArrow',  mods = 'CTRL|ALT', action = wezterm.action.AdjustPaneSize { 'Down', 1 } },
+  { key = 'LeftArrow',  mods = 'ALT', action = wezterm.action.AdjustPaneSize { 'Left', 1 } },
+  { key = 'RightArrow', mods = 'ALT', action = wezterm.action.AdjustPaneSize { 'Right', 1 } },
+  { key = 'UpArrow',    mods = 'ALT', action = wezterm.action.AdjustPaneSize { 'Up', 1 } },
+  { key = 'DownArrow',  mods = 'ALT', action = wezterm.action.AdjustPaneSize { 'Down', 1 } },
 }
 
 -- Finally, return the configuration to wezterm:
